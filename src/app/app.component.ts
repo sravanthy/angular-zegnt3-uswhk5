0184fe200da03ec1f5464e154fac7919c18b4565
sravanthy <cherukuri.sravanti@gmail.com>
2020-12-03 12:12:39 +0530

    Fetching from product list

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,29 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
-export class AppComponent  {}
+export class AppComponent {}
 
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at https://angular.io/license
+*/
+/*
+import { Component } from "@angular/core";
+import { products } from "./products";
+@Component({
+  selector: "app-product-list",
+  templateUrl: "./product-list/product-list.component.html",
+  styleUrls: ["./product-list/product-list.component.css"]
+})
+export class AppComponent {
+  private products = products;
+  myVar: any;
+}
 
 /*
 Copyright Google LLC. All Rights Reserved.
